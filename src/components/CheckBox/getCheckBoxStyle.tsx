f7f837c4477c522218e807ce1dd0404300d3880d
dousdogukan <dousdogukan@gmail.com>
2025-04-05 06:36:36 +0330

    chore: create checkbox component

---
+++ src/components/CheckBox/getCheckBoxStyle.tsx
@@ -0,0 +1,33 @@
+import { CheckboxType } from '.';
+
+export const getCheckBoxStyle = (
+  type: CheckboxType,
+  checked: boolean,
+  disabled: boolean,
+) => {
+  switch (type) {
+    case 'primary': {
+      return `w-5 h-5 ${
+        disabled ? 'pointer-events-none opacity-70' : 'cursor-pointer'
+      }
+      ${
+        checked
+          ? 'bg-[#1B59F8] border !border-[#1B59F8]'
+          : 'bg-white border border-gray'
+      }`;
+    }
+
+    case 'secondary': {
+      return `w-5 h-5 ${
+        disabled
+          ? 'pointer-events-none !bg-[#F2F4F7] border !border-[#EAECF0]'
+          : 'cursor-pointer'
+      }
+      ${
+        checked
+          ? 'bg-[#EEFFF9] border !border-[#1B59F8]'
+          : ' bg-white border !border-gray'
+      }`;
+    }
+  }
+};
