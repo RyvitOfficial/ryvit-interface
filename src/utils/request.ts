3ce1ef011bcbe04934dcd2de755b2b498de8b328
dousdogukan <dousdogukan@gmail.com>
2025-04-05 06:33:56 +0330

    chore: create request

---
+++ src/utils/request.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { ApiResponse } from '@/types';
+
+interface RequestConfig extends Omit<RequestInit, 'body'> {
+  body?: Record<string, any> | string;
+}
+
+const request = async <T>(
+  url: string,
+  config: RequestConfig = {},
+): Promise<{ data: ApiResponse<T>; response: Response }> => {
+  const { headers, body, ...restConfig } = config;
+
+  const defaultHeaders = {
+    'Content-Type': 'application/json',
+    ...headers,
+  };
+
+  const params: RequestInit = {
+    ...restConfig,
+    headers: defaultHeaders,
+  };
+
+  if (body) {
+    params.body = JSON.stringify(body);
+  }
+  const response = await fetch(url, params);
+
+  if (response.status >= 400) {
+    const data = await response.json();
+    throw { data, response };
+  }
+
+  const data = await response.json();
+  return { data, response };
+};
+
+export default request;
