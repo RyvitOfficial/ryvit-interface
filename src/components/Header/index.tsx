894bb40dfaf630c5380c417831560b3cd32e7211
dousdogukan <dousdogukan@gmail.com>
2025-04-08 03:56:47 +0330

    chore: add liveLedger to header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,36 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+import { getChangedDigits } from '@/utils/getChangerNumber';
+import { getLatestLedger } from '@/utils/getLatestLedger';
+
 interface HeaderProps {
   title: string;
 }
 
 const Header = ({ title }: HeaderProps) => {
+  const [lastLedger, setLastLedger] = useState<string>('0');
+  const [previousLedger, setPreviousLedger] = useState<string>('324233');
+
+  useEffect(() => {
+    const data = () => {
+      getLatestLedger().then((ledger) => {
+        setPreviousLedger(lastLedger);
+        setLastLedger(ledger.toString());
+      });
+    };
+
+    data();
+
+    const intervalId = setInterval(data, 2000);
+
+    return () => clearInterval(intervalId);
+  }, [lastLedger]);
+
+  const changedDigits = getChangedDigits(previousLedger, lastLedger);
+
   return (
     <header className="w-full bg-white rounded-[13px] border-2 border-border flex items-center justify-between h-[100px] px-8">
       <span className="text-2xl text-[#343C6A] font-[600]">{title}</span>
@@ -16,7 +44,27 @@ const Header = ({ title }: HeaderProps) => {
           </div>
 
           <div className="bg-[#E9EFFF] px-3 py-2 h-full">
-            <span className="text-[13px] text-primary">23456549</span>
+            <span className="text-[13px] text-primary">
+              {lastLedger.split('').map((digit, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 1 }}
+                  animate={{ opacity: changedDigits.includes(index) ? 0 : 1 }}
+                  transition={{
+                    duration: 0.01,
+                    type: 'tween',
+                    ease: 'easeInOut',
+                  }}
+                  className={
+                    changedDigits.includes(index)
+                      ? 'text-blue-400'
+                      : 'text-primary'
+                  }
+                >
+                  {digit}
+                </motion.span>
+              ))}
+            </span>
           </div>
         </div>
 
