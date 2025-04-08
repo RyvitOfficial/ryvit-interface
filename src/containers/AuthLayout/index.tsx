bb079b491d15d3e1063efb3b6c96dde34f48b6f2
dousdogukan <dousdogukan@gmail.com>
2025-04-08 03:54:32 +0330

    chore: add Loading

--- src/containers/AuthLayout/index.tsx
+++ src/containers/AuthLayout/index.tsx
@@ -1,10 +1,48 @@
 'use client';
 
+import { useRouter } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
+import LoadingProgressBar from '@/components/Loading';
+
+import { useAppSelector } from '@/hooks/useRedux';
+
 interface IAuthLayout {
   children: React.ReactNode;
 }
 
 const AuthLayout = ({ children }: IAuthLayout) => {
+  const router = useRouter();
+  const isLogin = useAppSelector((state) => state.user.isLogin);
+
+  const [isMounted, setIsMounted] = useState(false);
+  const [showLoading, setShowLoading] = useState(true);
+
+  useEffect(() => {
+    setIsMounted(true);
+
+    const timer = setTimeout(() => {
+      setShowLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  useEffect(() => {
+    if (isMounted && isLogin) {
+      router.push('/dashboard');
+    }
+  }, [isLogin, router, isMounted]);
+
+  if (!isMounted || showLoading) {
+    return (
+      <div className="w-screen h-screen flex items-center justify-center bg-white">
+        <LoadingProgressBar />
+      </div>
+    );
+  }
+
+  if (isLogin) return null;
   return (
     <div className="w-full h-dvh flex justify-between items-center">
       <div className="absolute top-1/2 left-1/2 w-[1000px] h-full bg-[radial-gradient(circle,_rgba(27,89,248,0.5)_0%,_rgba(27,89,248,0)_70%)] blur-[150px] transform -translate-x-1/2 -translate-y-1/2 animate-pulse "></div>
