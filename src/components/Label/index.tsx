aead44c99739b856802b4dc5e1644338c5579170
dousdogukan <dousdogukan@gmail.com>
2025-04-10 07:51:20 +0330

    temp: add props

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -2,14 +2,17 @@ interface CLabelProps {
   label?: string;
   htmlFor?: string;
   className?: string;
+  noneMargin?: boolean;
 }
 
-const CLabel = ({ label, htmlFor, className }: CLabelProps) => {
+const CLabel = ({ label, htmlFor, className, noneMargin }: CLabelProps) => {
   return (
     <div className={`flex items-start ml-1  ${className}`}>
       <label
         htmlFor={htmlFor}
-        className="text-[15px] font-inter font-[400] text-[#343C6A] mb-2 flex"
+        className={`text-[15px] font-inter font-[400] text-[#343C6A] ${
+          noneMargin ? '' : 'mb-2 '
+        }flex`}
       >
         {label}
       </label>
