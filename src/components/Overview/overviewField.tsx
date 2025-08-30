96c09e5ff4658cdcd29a0d65aa37ce757a585460
dousdogukan <dousdogukan@gmail.com>
2025-08-30 19:48:27 +0330

    style(OverView): dark theme

--- src/components/Overview/overviewField.tsx
+++ src/components/Overview/overviewField.tsx
@@ -1,3 +1,5 @@
+import cn from 'classnames';
+
 interface OverviewFieldProps {
   title: string;
   value: string;
@@ -6,29 +8,19 @@ interface OverviewFieldProps {
 
 const OverviewField = ({ title, value, type }: OverviewFieldProps) => {
   return (
-    <div className="w-full flex justify-between items-center">
-      <div className="flex justify-center items-center space-x-3">
+    <div className="w-full flex justify-between items-center bg-input/40 px-4 py-3 desktopMax:py-2 rounded-lg text-[15px] desktopMax:text-xs">
+      <div className="flex justify-center items-center space-x-3 ">
         <div
           className={`${
             type === 'white' ? 'bg-white' : 'bg-primary'
           } w-2 h-2 rounded-full`}
         ></div>
-        <p
-          className={`${
-            type === 'white' ? 'text-white' : 'text-secondary'
-          } text-[14px]`}
-        >
+        <p className={cn(type === 'white' ? 'text-white' : 'text-txtgray')}>
           {title}
         </p>
       </div>
       <div>
-        <p
-          className={`${
-            type === 'white' ? 'text-white' : 'text-[#718EBF]'
-          } text-[14px]`}
-        >
-          {value}
-        </p>
+        <p className=" text-white font-jetbrains">{value}</p>
       </div>
     </div>
   );
