e0143146cfc033ccfba76bdb255a61f00ab9f90b
dousdogukan <dousdogukan@gmail.com>
2025-09-01 02:38:51 +0330

    feat: add filter card

---
+++ src/containers/ContractsContainer/FilterCard.tsx
@@ -0,0 +1,44 @@
+import { Add, Search } from '@/assets';
+import Button from '@/components/Button';
+import Card from '@/components/Card';
+import CInput from '@/components/Input';
+
+interface FilterCardProps {
+  search: string;
+  setSearch: (value: string) => void;
+  onAddContract?: () => void;
+}
+
+const FilterCard = ({ search, setSearch, onAddContract }: FilterCardProps) => {
+  return (
+    <Card
+      bgColor="#121319"
+      borderColor="transparent"
+      className="w-full text-white p-4 flex justify-between items-center"
+    >
+      <div className="w-2/6">
+        <CInput
+          type="text"
+          placeholder="Search Contracts ..."
+          inputClassName="!border-border2 font-jetbrains text-txtgray desktopMax:rounded-lg desktopMax:h-[35px] desktopMax:text-[13px]"
+          border
+          icon={<Search fill="#9CA3AF" />}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+
+      <div>
+        <Button
+          color="blue"
+          content="Add Contract"
+          rounded="sm"
+          className="!h-12 desktopMax:!h-[35px] desktopMax:text-sm"
+          onClick={onAddContract}
+          logo={<Add />}
+        />
+      </div>
+    </Card>
+  );
+};
+
+export default FilterCard;
