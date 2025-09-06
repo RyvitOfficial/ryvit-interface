900ecb307b8c59d15de1202358c804b6573dcb67
dousdogukan <dousdogukan@gmail.com>
2025-09-06 09:00:21 +0330

    fix: enhance baseTransaction to support multiple operation types and improve Soroban data handling

--- src/utils/soroban/baseTrasaction.ts
+++ src/utils/soroban/baseTrasaction.ts
@@ -4,14 +4,22 @@ import {
   Networks,
   Operation,
   TransactionBuilder,
+  SorobanDataBuilder,
+  Memo,
 } from '@stellar/stellar-sdk';
 
 import getConfig from '../getConfig';
+import { NetworkType } from '@/types';
 
 const baseTransaction = async (
   admin: Account,
-  call: xdr.Operation<Operation.Payment>,
-  network: 'testnet',
+  call:
+    | xdr.Operation<Operation.ExtendFootprintTTL>
+    | xdr.Operation<Operation.RestoreFootprint>
+    | xdr.Operation<Operation.Payment>,
+  network: NetworkType,
+  keys?: xdr.LedgerKey[],
+  type?: 'extend' | 'restore',
 ) => {
   const { fee } = await getConfig(network);
 
@@ -20,9 +28,24 @@ const baseTransaction = async (
     networkPassphrase: Networks.TESTNET,
   });
 
+  if (keys) {
+    if (type === 'extend') {
+      transaction = transaction.setSorobanData(
+        new SorobanDataBuilder().setFootprint(keys).build(),
+      );
+    } else {
+      transaction = transaction.setSorobanData(
+        new SorobanDataBuilder().setReadWrite(keys).build(),
+      );
+    }
+  }
+
   transaction = transaction.addOperation(call);
 
-  transaction = transaction.setTimeout(30);
+  transaction = transaction.addMemo(Memo.text('Manage TTL https://ryvit.app'));
+
+  transaction = transaction.setTimeout(0);
+
   const transactionBuild = transaction.build();
 
   return transactionBuild;
