8d9c2bf73d5e3908130c831db5db1c686ed07e5a
dousdogukan <dousdogukan@gmail.com>
2025-09-08 11:13:47 +0330

    style: change colors

--- src/containers/UserMenu/index.tsx
+++ src/containers/UserMenu/index.tsx
@@ -14,17 +14,19 @@ const UserMenu = ({ children }: IUserMenu) => {
   const dispatch = useDispatch();
 
   return (
-    <Dropdown trigger={children}>
-      <div className="px-4 py-2 text-sm text-gray-700">
-        <p className="font-medium">{user?.name}</p>
-        <p className="text-gray-500 truncate">{user?.email}</p>
-        <p className="text-gray-500 truncate">
+    <Dropdown trigger={children} className="overflow-hidden">
+      <div className="px-4 py-2 text-sm space-y-1">
+        <p className="font-medium text-white truncate">{user?.name}</p>
+        <p className="text-txtgray truncate">{user?.email}</p>
+        <p className="text-txtgray truncate">
           Balance : {`${user && user.balanceTest / 10 ** 7} XLM`}
         </p>
       </div>
-      <hr className="border-gray-200" />
+
+      <hr className="border-border2/50" />
+
       <button
-        className="w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-gray-100"
+        className="w-full px-4 py-2 text-left text-sm text-red-600 hover:bg-gray-900"
         onClick={() => {
           dispatch(logout());
         }}
