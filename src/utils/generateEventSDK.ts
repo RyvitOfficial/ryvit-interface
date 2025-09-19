1fa5a57aeef023e1ff3d8ed420216b90a927b38d
dousdogukan <dousdogukan@gmail.com>
2025-09-20 03:10:36 +0330

    refactor: restructure event interfaces and improve type definitions in generateEventSDK

--- src/utils/generateEventSDK.ts
+++ src/utils/generateEventSDK.ts
@@ -1,34 +1,41 @@
 import { IGetContractResponse } from '@/types';
+const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
 
 const generateEventSDK = (currentContract: IGetContractResponse) => {
-  const iEventString = currentContract.event.events
-    .filter((e) => e.selected)
-    .map((e) => `${e.name}: IPayload`)
-    .join('\n    ');
-
+  const events = currentContract.event.events.filter((e) => e.selected);
   const handlersString = currentContract.event.events
     .filter((e) => e.selected)
     .map((e) => `${e.name}: (data) => { console.log(data) }`)
     .join(',\n        ');
+
+  const IEventString =
+    events.length != 0
+      ? ` ${'\n'}interface IEvent {
+${events.map((e) => `    ${e.name}: I${capitalize(e.name)}Payload`).join('\n')}
+} ${'\n'}`
+      : '';
+
+  const IBase =
+    events
+      .map(
+        (e) => `
+      ${'\n'}interface I${capitalize(e.name)}Payload extends IPayloadBase {
+    value: ${e.valueType.split('|').join('|\n')};
+    params: [${e.paramsType.join(', ')}];
+}`,
+      )
+      .join('\n') + '\n';
 
   return `
 import { eventSetup } from 'ryvit';
 import { Application } from 'express';
 import { rpc } from '@stellar/stellar-sdk';
 
-
-interface IPayload {
+interface IPayloadBase {
     event: rpc.Api.EventResponse,
     ledger: number,
     hash: string,
-    value: any,
-    params: any[]
-};
-
-interface IEvent {
-    ${iEventString}
-}
-
+};${IBase}${IEventString}
 const handleEvents = (app: Application) => {
   eventSetup<IEvent>(app, {
       route: '/example',
